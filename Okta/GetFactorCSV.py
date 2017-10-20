5493e8fecc5786eef7eed627f0be3162845dfcee
time4116 <31143754+time4116@users.noreply.github.com>
2017-10-20 15:02:46 -0700

    Updated formatting

--- Okta/GetFactorCSV.py
+++ Okta/GetFactorCSV.py
@@ -1,41 +1,37 @@
 from okta import UsersClient
 from okta import FactorsClient
 
-usersClientdev = UsersClient(site, skey)
+site = 'Your Site'
+skey = 'Your Key'
+
+usersClient = UsersClient(site, skey)
+factorClient = FactorsClient(site, skey)
+
+currentDate = time.strftime('%Y%m%d')
 
 lst = []
 users = usersClient.get_paged_users()
 while True:
     for user in users.result:
-        if user.status == 'ACTIVE':
+        if user.status == 'ACTIVE': # Get only active users
             lst.append(user)
-            #lst.append(user.profile.login)
-
+            
     if not users.is_last_page():
         # Keep on fetching pages of users until the last page
         users = usersClient.get_paged_users(url=users.next_url)
     else:
         break
 
-currentDate = time.strftime('%Y%m%d')
-faccli = FactorsClient(site, skey)
-
 EnrolledUsers = open('C:\\temp\\EnrolledUsers'+currentDate+'.csv', 'w+')
 EnrolledUsers.write("Username,FactorType,UserID,DateEnrolled,TimeEnrolled")
 
 for user in lst:
-    cat = faccli.get_lifecycle_factors(user_id=user.id)
-    # print("\n")
-    # print(user.profile.login)
-    for i in cat:
-        #createdtime = str(i.created)
-        #timesplit = createdtime.split(' ')[0]
-        #total = datetime.strptime(timesplit, "%Y-%m-%d")
-
-        if i.factorType == 'sms': # total >= date1 and
+    lifecycle = factorClient.get_lifecycle_factors(user_id=user.id)
+   
+    for life in lifecycle:
+        if life.factorType == 'sms': # Select factor type here
             EnrolledUsers.write('\n')
-            # print(user.profile.login)
-            time = str(i.created)
+             time = str(i.created)
             timecreated = time1.split()[1]
             datecreated = time1.split()[0]
 
